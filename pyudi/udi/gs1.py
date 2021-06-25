38a1944ddd6b3706567d3c4692c28911557d4dd8
Fabricio Roberto Reinert <fabricio.reinert@live.com>
2021-06-24 21:50:50 -0300

    - Removed Humanized convertion from GS1 - Will only support regex for barcode string

--- pyudi/udi/gs1.py
+++ pyudi/udi/gs1.py
@@ -8,25 +8,16 @@ class GS1Pattern(IBaseUDI):
     encoded_udi_string = ''
     human_readable_udi_string = ''
 
-    def __init__(self, barcode: str = None, human_readable: str = None):
-        if barcode or human_readable:
-            self.encode_udi(barcode=barcode, human_readable=human_readable)
+    def __init__(self, barcode: str):
+        self.encode_udi(barcode = barcode)
 
-    def encode_udi(self, barcode: str = None, human_readable: str = None) -> None:
-        '''Try to encode data from humanized/barcode string'''
+    def encode_udi(self, barcode: str) -> None:
+        '''Encode data from barcode string'''
+            
+        for field in self.field_list(gs1):
+            # Barcodes in GS1 dont use '(' or ')' within the string
+            pass
 
-        if barcode:
-            for field in self.field_list(gs1):
-                # Barcodes in GS1 dont use '(' or ')' within the string
-                pass
-
-        elif human_readable:
-            for field in self.field_list(gs1):
-                # Human readable UDI in GS1 use '(' and ')' to encapsulate data limiter
-                pass
-
-        else:
-            raise TypeError('Missing parameters: <barcode> OR <human_readable>')
 
     def decode_udi(self) -> str:
         '''Convert instance in barcode/humanized string'''
