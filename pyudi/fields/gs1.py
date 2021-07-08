5d0cf69a9c370283b2263769aa7d0c70c5ca9c2f
Fabricio R Reinert <fabricio.reinert@live.com>
2021-07-08 17:24:55 -0300

    refactoring

--- pyudi/fields/gs1.py
+++ pyudi/fields/gs1.py
@@ -1,5 +1,6 @@
 '''GS1 fields implementation'''
 
+from pyudi.common import Agency
 from pyudi.fields.base import IField
 
 
@@ -9,14 +10,14 @@ __all__ = ['SSCCField', 'GTINField', 'ContentField', 'BatchLotField', 'Productio
 
 
 '''
-Base Fields
+    Generic GS1 Fields
 '''
 
 
 class GS1AlphanumericField(IField):
     '''Base GS1 Alphanumeric Field'''
 
-    def __init__(self, agency: str, value: str) -> None:
+    def __init__(self, agency: Agency, value: str) -> None:
         self.agency = agency
         self.value = value
 
@@ -33,7 +34,7 @@ class GS1AlphanumericField(IField):
 class GS1DateField(IField):
     '''GS1 Date Field'''
 
-    def __init__(self, agency: str, value: int) -> None:
+    def __init__(self, agency: Agency, value: int) -> None:
         self.agency = agency
         self.value = value
 
@@ -45,7 +46,7 @@ class GS1DateField(IField):
 class GS1NumericField(IField):
     '''GS1 Numeric Field'''
 
-    def __init__(self, agency: str, value: int) -> None:
+    def __init__(self, agency: Agency, value: int) -> None:
         self.agency = agency
         self.value = value
 
@@ -55,14 +56,14 @@ class GS1NumericField(IField):
 
 
 '''
-GS1 Fields
+    GS1 Fields
 '''
 
 
 class SSCCField(GS1NumericField):
     '''00 Serial Shipping Container Code (SSCC)'''
 
-    agency = 'gs1'
+    agency: Agency = Agency.GS1
     name = 'field_SSCC'
     data_delimiter = '00'
     data_size = 18
@@ -71,7 +72,7 @@ class SSCCField(GS1NumericField):
 class GTINField(GS1NumericField):
     '''01 Global Trade Item Number (GTIN)'''
 
-    agency = 'gs1'
+    agency: Agency = Agency.GS1
     name = 'field_GTIN'
     data_delimiter = '01'
     data_size = 14
@@ -80,7 +81,7 @@ class GTINField(GS1NumericField):
 class ContentField(GS1NumericField):
     '''02 Global Trade Item Number (GTIN) of contained trade items'''
 
-    agency = 'gs1'
+    agency: Agency = Agency.GS1
     name = 'field_CONTENT'
     data_delimiter = '02'
     data_size = 14
@@ -89,7 +90,7 @@ class ContentField(GS1NumericField):
 class BatchLotField(GS1AlphanumericField):
     '''10 Batch or lot number'''
 
-    agency = 'gs1'
+    agency: Agency = Agency.GS1
     name = 'field_BATCH_LOT'
     data_delimiter = '10'
     data_size = 20
@@ -98,7 +99,7 @@ class BatchLotField(GS1AlphanumericField):
 class ProductionDateField(GS1DateField):
     '''11 Production date (YYMMDD)'''
 
-    agency = 'gs1'
+    agency: Agency = Agency.GS1
     name = 'field_PROD_DATE'
     data_delimiter = '11'
     data_size = 6
@@ -107,7 +108,7 @@ class ProductionDateField(GS1DateField):
 class ExpiringDateField(GS1DateField):
     '''12 Due date (YYMMDD)'''
 
-    agency = 'gs1'
+    agency: Agency = Agency.GS1
     name = 'field_DUE_DATE'
     data_delimiter = '12'
     data_size = 6
@@ -116,7 +117,7 @@ class ExpiringDateField(GS1DateField):
 class PackingDateField(GS1DateField):
     '''13 Packaging date (YYMMDD)'''
 
-    agency = 'gs1'
+    agency: Agency = Agency.GS1
     name = 'field_PACK_DATE'
     data_delimiter = '13'
     data_size = 6
@@ -125,7 +126,7 @@ class PackingDateField(GS1DateField):
 class MinimalExpiringField(GS1DateField):
     '''15 Best before date (YYMMDD)'''
 
-    agency = 'gs1'
+    agency: Agency = Agency.GS1
     name = 'field_BEST_BEFORE_OR_BEST_BY'
     data_delimiter = '15'
     data_size = 6
@@ -134,7 +135,7 @@ class MinimalExpiringField(GS1DateField):
 class SellExpiringField(GS1DateField):
     '''16 Sell by date (YYMMDD)'''
 
-    agency = 'gs1'
+    agency: Agency = Agency.GS1
     name = 'field_SELL_BY'
     data_delimiter = '16'
     data_size = 6
@@ -143,7 +144,7 @@ class SellExpiringField(GS1DateField):
 class MaxExpiringDateField(GS1DateField):
     '''17 Expiration date (YYMMDD)'''
 
-    agency = 'gs1'
+    agency: Agency = Agency.GS1
     name = 'field_USE_BY_OR_EXPIRY'
     data_delimiter = '17'
     data_size = 6
@@ -152,7 +153,7 @@ class MaxExpiringDateField(GS1DateField):
 class InternalProductVariantField(GS1NumericField):
     '''20 Internal product variant'''
 
-    agency = 'gs1'
+    agency: Agency = Agency.GS1
     name = 'field_VARIANT'
     data_delimiter = '20'
     data_size = 2
@@ -161,7 +162,7 @@ class InternalProductVariantField(GS1NumericField):
 class SerialNumberField(GS1AlphanumericField):
     '''21 Serial number'''
 
-    agency = 'gs1'
+    agency: Agency = Agency.GS1
     name = 'field_SERIAL'
     data_delimiter = '21'
     data_size = 20
@@ -170,7 +171,7 @@ class SerialNumberField(GS1AlphanumericField):
 class ConsumerProductVariantField(GS1AlphanumericField):
     '''21 Serial number'''
 
-    agency = 'gs1'
+    agency: Agency = Agency.GS1
     name = 'field_CPV'
     data_delimiter = '22'
     data_size = 20
--- pyudi/udi/gs1.py
+++
@@ -1,5 +0,0 @@
-from pyudi.udi.base import BaseUDI
-
-
-class StructureGS1(BaseUDI):
-    agency: str = 'GS1'
